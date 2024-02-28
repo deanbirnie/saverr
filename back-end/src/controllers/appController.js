9386a65face57adff11dd313df3f31af682fd899
deanbirnie <deanicdl@gmail.com>
2024-02-28 17:00:09 +0200

    Refactor getBudgetInfo function and add route for retrieving budget information

--- back-end/src/controllers/appController.js
+++ back-end/src/controllers/appController.js
@@ -15,7 +15,7 @@ export const getBudgets = async (req, res) => {
         userId: user.id,
       },
     });
-    if (Object.keys(allBudgets).length === 0) {
+    if (allBudgets.length === 0) {
       return res.status(404).json({ message: "No budgets found." });
     }
     return res.status(200).json(allBudgets);
@@ -51,7 +51,7 @@ export const createBudget = async (req, res) => {
         year: req.body.year,
       },
     });
-    if (Object.keys(newBudget).length === 0) {
+    if (newBudget.length === 0) {
       return res.status(401).json({ message: "Couldn't create budget." });
     }
     return res.status(201).json(newBudget);
@@ -69,4 +69,40 @@ export const createBudget = async (req, res) => {
   }
 };
 
-export const getBudgetInfo = () => {};
+export const getBudgetInfo = async (req, res) => {
+  const userEmail = req.user;
+  const budgetId = parseInt(req.query.id, 10);
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: userEmail,
+      },
+    });
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+    if (isNaN(budgetId)) {
+      return res.status(400).json({ message: "Invalid budget ID." });
+    }
+    const budget = await prisma.budget.findUnique({
+      where: {
+        id: budgetId,
+      },
+    });
+    if (!budget) {
+      return res.status(404).json({ message: "Budget not found." });
+    }
+    return res.status(200).json(budget);
+  } catch (err) {
+    console.log(err.message);
+    return res.status(500).json({ error: "Internal server error." });
+  } finally {
+    console.log("Disconnecting client...");
+    try {
+      await prisma.$disconnect();
+      console.log("Client disconnected successfully.");
+    } catch (err) {
+      console.error("Couldn't disconnect client: " + err.message);
+    }
+  }
+};
--- back-end/src/routes/appRoutes.js
+++ back-end/src/routes/appRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { getBudgets, createBudget } from "../controllers/appController.js";
+import {
+  getBudgets,
+  createBudget,
+  getBudgetInfo,
+} from "../controllers/appController.js";
 import { authenticateToken } from "../middleware/authenticateToken.js";
 
 export const appRouter = express.Router();
 
 appRouter.get("/find-budgets", authenticateToken, getBudgets);
 appRouter.post("/create-budget", authenticateToken, createBudget);
+appRouter.get("/get-budget-info", authenticateToken, getBudgetInfo);
